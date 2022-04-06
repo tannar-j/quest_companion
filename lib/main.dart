647335aaadfab52c3b6880baa0ce81bb7eb8fb7d
Tannar <tannar.james@gmail.com>
2022-04-05 20:21:17 -0500

    Main Color
    Change color

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       title: 'Quest Companion',
       theme: ThemeData(
         fontFamily: 'Ringbearer',
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const DefaultTabController(
           length: 3,
